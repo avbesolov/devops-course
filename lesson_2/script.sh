c24de177998b9b22d5d04e3380fce08561bf9b2d
avbesolov <avbesolov@sberbank.ru>
2022-12-06 16:04:05 +0300

    script

--- lesson_2/script.sh
+++ lesson_2/script.sh
@@ -5,9 +5,9 @@ for i in $(seq 1 5)
 do 
     base64 /dev/urandom | head -c 100000 > file$i.txt
 done
-echo "\n===\nDone!!!\n==="
 
-# Вывод содержимого папки
-echo "\nСодержимое папки lesson_2\n"
-ls -lah .
-echo
+# Передача непустых сгенерированных файлов на удаленный сервер 
+rsync -avz --progress ~/Projects/repos/devops-course/lesson_2/files/ root@195.2.81.60:~/projects/lesson_2/files
+
+# Успешность выполнения скрипта
+echo "===\nDone!!!\n==="
